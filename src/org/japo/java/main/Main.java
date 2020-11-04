e0ce6307c3c35d8bb8443fdaa126b3b3f30ec640
Julian David Ramos Gomez <juli12319@hotmail.com>
2020-11-04 20:13:56 +0100

    Hola Mundo Constante

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CicloM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        final String SALUDO_CONSTANTE = "¡Hola Mundo!";
+        System.out.println(SALUDO_CONSTANTE);
+    }
+}
